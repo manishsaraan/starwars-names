ab51d6ab72357ce10da4443984ab1c9cf18269a6
manishsaraan <manish021js@gmail.com>
2018-03-11 12:50:56 +0530

    style(random): Add ability to get number of random items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var uniqueRandomArray = require('unique-random-array');
 var starWarsName = require('./starwars-names.json');
+var getRandomItem = uniqueRandomArray(starWarsName);
 
+function random(number){
+  if(number === undefined){
+    return getRandomItem();
+  }
+  else{
+    var randomItems = [];
+    for(var i = 0; i< number; i++){
+    	randomItems.push(getRandomItem());
+    }
+    return randomItems;
+  }
+}
 module.exports = {
 	all : starWarsName,
-	random : uniqueRandomArray(starWarsName)
+	random : random
 };
