d1653ba25b67e22e921d3651d44ae7f7101c9d02
manishsaraan <manish021js@gmail.com>
2018-03-17 15:18:11 +0530

    feat: ES6 support added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-var uniqueRandomArray = require('unique-random-array');
-var starWarsName = require('./starwars-names.json');
-var getRandomItem = uniqueRandomArray(starWarsName);
+import uniqueRandomArray from 'unique-random-array';
+import starWarsName from './starwars-names.json';
+const getRandomItem = uniqueRandomArray(starWarsName);
 
 function random(number){
   if(number === undefined){
